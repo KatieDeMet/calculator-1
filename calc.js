a7d81aa0103d8c07d39f2a820b3e72cf9c2c601d
Katie DeMet <katie.demet7@gmail.com>
2021-07-12 15:09:55 -0500

    Still buggy but updated version of adding 3rd number

--- calc.js
+++ calc.js
@@ -16,23 +16,35 @@ reader.question("What would you like to calculate?", function(input){
 	num3 = Number(tokens[3]);
 
 	if (mathSymbol === '+'){
+		console.log(num1 + num2);
 		console.log(num1 + num2 + num3);
 	} else if (mathSymbol === '-'){
+		console.log(num1 - num2);
 		console.log(num1 - num2 - num3);
 	} else if (mathSymbol === '*') {
+		console.log(num1 * num2);
 		console.log(num1 * num2 * num3);
 	} else if (mathSymbol === '/') {
+		console.log(num1 / num2);
 		console.log(num1 / num2 / num3);
 	} else if (mathSymbol === 'sqrt') {
 		console.log(Math.sqrt(num1));
+		console.log(Math.sqrt(num2));
+		console.log(Math.sqrt(num3));
 	} else if (mathSymbol === 'square') {
 		console.log(num1 * num1);
+		console.log(num2 * num2);
+		console.log(num3 * num3);
 	} else if (mathSymbol === 'cube') {
 		console.log(num1 * num1 * num1);
+		console.log(num2 * num2 * num2);
+		console.log(num3 * num3 * num3);
 	} else if (mathSymbol === '^') {
 		console.log(Math.pow(num1, num2));
+		console.log(Math.pow(Math.pow(num1, num2)), num3);
 	} else if (mathSymbol === '%') {
-		console.log(num1 % num2);
+			console.log(num1 % num2);
+			console.log(num1 % num2 % num3);
 	} 
 	else {
 		console.log('The calculator does not recognize your math symbol.')
